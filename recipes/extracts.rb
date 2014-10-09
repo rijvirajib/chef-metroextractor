ddb83e78d0e112fe2a328ea2fb550f20c6ebfb5e
Grant Heffernan <heffergm@gmail.com>
2014-10-09 07:55:28 -0400

    lockfiles

--- recipes/extracts.rb
+++ recipes/extracts.rb
@@ -3,19 +3,31 @@
 # Recipe:: extracts
 #
 
+file "#{node[:metroextractor][:setup][:basedir]}/.vexdb.lock" do
+  action :nothing
+end
+
 bash 'create vexdb' do
-  user    node[:metroextractor][:user][:id]
-  cwd     node[:metroextractor][:setup][:basedir]
-  timeout node[:metroextractor][:vex][:db_timeout]
+  user      node[:metroextractor][:user][:id]
+  cwd       node[:metroextractor][:setup][:basedir]
+  timeout   node[:metroextractor][:vex][:db_timeout]
+  notifies  :create, "file[#{node[:metroextractor][:setup][:basedir]}/.vexdb.lock]", :immediately
+  not_if    { ::File.exist?("#{node[:metroextractor][:setup][:basedir]}/.vexdb.lock") }
   code <<-EOH
     vex #{node[:metroextractor][:vex][:db]} #{node[:metroextractor][:planet][:basedir]}/#{node[:metroextractor][:planet][:file]}
   EOH
 end
 
+file "#{node[:metroextractor][:setup][:basedir]}/.extracts.lock" do
+  action :nothing
+end
+
 bash 'create extracts' do
-  user    node[:metroextractor][:user][:id]
-  cwd     node[:metroextractor][:setup][:basedir]
-  timeout node[:metroextractor][:extracts][:extracts_timeout]
+  user      node[:metroextractor][:user][:id]
+  cwd       node[:metroextractor][:setup][:basedir]
+  timeout   node[:metroextractor][:extracts][:extracts_timeout]
+  notifies  :create, "file[#{node[:metroextractor][:setup][:basedir]}/.extracts.lock]", :immediately
+  not_if    { ::File.exist?("#{node[:metroextractor][:setup][:basedir]}/.extracts.lock") }
   code <<-EOH
     parallel --jobs #{node[:metroextractor][:vex][:jobs]} -a #{node[:metroextractor][:setup][:scriptsdir]}/extracts.sh --joblog #{node[:metroextractor][:setup][:basedir]}/logs/parallel_extracts.log
   EOH
--- recipes/shapes.rb
+++ recipes/shapes.rb
@@ -3,9 +3,15 @@
 # Recipe:: shapes
 #
 
+file "#{node[:metroextractor][:setup][:basedir]}/.shapes.lock" do
+  action :nothing
+end
+
 execute 'osm2pgsql' do
-  user     node[:metroextractor][:user][:id]
-  cwd      node[:metroextractor][:setup][:basedir]
-  command  "#{node[:metroextractor][:setup][:scriptsdir]}/osm2pgsql.sh"
-  timeout  node[:metroextractor][:shapes][:osm2pgsql_timeout]
+  user      node[:metroextractor][:user][:id]
+  cwd       node[:metroextractor][:setup][:basedir]
+  command   "#{node[:metroextractor][:setup][:scriptsdir]}/osm2pgsql.sh"
+  timeout   node[:metroextractor][:shapes][:osm2pgsql_timeout]
+  notifies  :create, "file[#{node[:metroextractor][:setup][:basedir]}/.shapes.lock]", :immediately
+  not_if    { ::File.exist?("#{node[:metroextractor][:setup][:basedir]}/.shapes.lock") }
 end
