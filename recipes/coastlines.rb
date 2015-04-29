dccf75d0d9267fe98c0792f439a7be1aed0df458
Grant Heffernan <heffergm@gmail.com>
2015-04-29 07:32:56 -0400

    fix

--- recipes/coastlines.rb
+++ recipes/coastlines.rb
@@ -3,30 +3,30 @@
 # Recipe:: coastlines
 #
 
-bash 'wget water polygons' do
+execute 'wget water polygons' do
   user node[:metroextractor][:user][:id]
   cwd  node[:metroextractor][:setup][:basedir]
-  code <<-EOH
+  command <<-EOH
     wget --quiet -O #{node[:coastlines][:water_polygons][:file]} #{node[:coastlines][:water_polygons][:url]} && unzip #{node[:coastlines][:water_polygons][:file]}
   EOH
   not_if { ::File.exist?("#{node[:metroextractor][:setup][:basedir]}/#{node[:coastlines][:water_polygons][:file]}") }
   only_if { node[:metroextractor][:coastlines][:process] == true }
 end
 
-bash 'wget land polygons' do
+execute 'wget land polygons' do
   user node[:metroextractor][:user][:id]
   cwd  node[:metroextractor][:setup][:basedir]
-  code <<-EOH
+  command <<-EOH
     wget --quiet -O #{node[:coastlines][:land_polygons][:file]} #{node[:coastlines][:land_polygons][:url]} && unzip #{node[:coastlines][:land_polygons][:file]}
   EOH
   not_if { ::File.exist?("#{node[:metroextractor][:setup][:basedir]}/#{node[:coastlines][:land_polygons][:file]}") }
   only_if { node[:metroextractor][:coastlines][:process] == true }
 end
 
-bash 'generate coastlines' do
+execute 'generate coastlines' do
   user node[:metroextractor][:user][:id]
   cwd  node[:metroextractor][:setup][:basedir]
-  code <<-EOH
+  command <<-EOH
     #{node[:metroextractor][:setup][:scriptsdir]}/coastlines.sh >#{node[:metroextractor][:setup][:basedir]}/logs/coastlines.log 2>&1
   EOH
   timeout node[:coastlines][:generate][:timeout]
--- recipes/extracts.rb
+++ recipes/extracts.rb
@@ -8,25 +8,25 @@ execute 'osmconvert planet' do
   user    node[:metroextractor][:user][:id]
   cwd     node[:metroextractor][:setup][:basedir]
   timeout node[:metroextractor][:extracts][:osmconvert_timeout]
-  code <<-EOH
+  command <<-EOH
     osmconvert #{node[:metroextractor][:planet][:file]} -o=planet.o5m > #{node[:metroextractor][:setup][:basedir]}/logs/osmconvert_planet.log 2>&1
   EOH
 end
 
 execute 'osmconvert cities' do
-  user node[:metroextractor][:user][:id]
-  cwd  node[:metroextractor][:setup][:basedir]
-  code <<-EOH
-    parallel -j #{node[:metroextractor][:extracts][:osmconvert_jobs]} -a #{node[:metroextractor][:setup][:scriptsdir]}/osmconvert.sh --joblog #{node[:metroextractor][:setup][:basedir]}/logs/parallel_osmconvert.log
-  EOH
+  user      node[:metroextractor][:user][:id]
+  cwd       node[:metroextractor][:setup][:basedir]
   timeout   node[:metroextractor][:extracts][:osmconvert_timeout]
   notifies  :run, 'execute[fix osmconvert perms]', :immediately
   only_if   { node[:metroextractor][:extracts][:process] == true }
+  command <<-EOH
+    parallel -j #{node[:metroextractor][:extracts][:osmconvert_jobs]} -a #{node[:metroextractor][:setup][:scriptsdir]}/osmconvert.sh --joblog #{node[:metroextractor][:setup][:basedir]}/logs/parallel_osmconvert.log
+  EOH
 end
 
 execute 'fix osmconvert perms' do
-  action  :nothing
-  user    node[:metroextractor][:user][:id]
-  cwd     node[:metroextractor][:setup][:basedir]
-  code    'chmod 644 ex/*'
+  action    :nothing
+  user      node[:metroextractor][:user][:id]
+  cwd       node[:metroextractor][:setup][:basedir]
+  command   'chmod 644 ex/*'
 end
